65db89fa4b69cc9694c845ccebbb697247c09a3c
Ishmam Hossain <ishmamhossain4@gmail.com>
2024-04-03 15:22:12 +0600

    Add new files and components

---
+++ components/CustomButton.tsx
@@ -0,0 +1,24 @@
+'use client'; // to turn this component into a client-side component
+import { CustomButtonProps } from '@/types';
+
+const CustomButton = ({
+  title,
+  containerStyles,
+  handleClick,
+  btnType,
+}: CustomButtonProps) => {
+  return (
+    <button
+      disabled={false}
+      type={btnType || 'button'}
+      className={`custom-btn ${containerStyles}`}
+      onClick={() => {
+        handleClick;
+      }}
+    >
+      <span className="flex-1">{title}</span>
+    </button>
+  );
+};
+
+export default CustomButton;
