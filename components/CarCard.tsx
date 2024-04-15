10399cf07f2738a4f2897b3b959775cd4efde9b4
Ishmam Hossain <ishmamhossain4@gmail.com>
2024-04-15 13:32:28 +0600

    Add CarDetails component and update CarCard to use it

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -5,7 +5,8 @@ import Image from 'next/image';
 
 import { CarProps } from '@/types';
 import CustomButton from './CustomButton';
-import { calculateCarRent } from '@/utils';
+import { calculateCarRent, generateCarImageUrl } from '@/utils';
+import CarDetails from './CarDetails';
 
 interface CarCardProps {
   car: CarProps;
@@ -34,7 +35,7 @@ const CarCard = ({ car }: CarCardProps) => {
 
       <div className="relative w-full h-40 my-3 object-contain">
         <Image
-          src="/hero.png"
+          src={generateCarImageUrl(car)}
           alt="car model"
           fill
           priority
@@ -71,6 +72,12 @@ const CarCard = ({ car }: CarCardProps) => {
           />
         </div>
       </div>
+
+      <CarDetails
+        isOpen={isOpen}
+        closeModal={() => setIsOpen(false)}
+        car={car}
+      />
     </div>
   );
 };
